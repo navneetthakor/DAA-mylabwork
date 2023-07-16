72540f79313845a0eafb418a9d5a92b96d0a7575
navneetthakor <tnavneer8628@gmail.com>
2023-07-16 11:13:54 +0530

    best & worst case added

--- lab1/turn1_main.cpp
+++ lab1/turn1_main.cpp
@@ -76,105 +76,150 @@ void ssort(vector<int> &v)
             v[i] = temp;
         }
     }
-
 }
 
 // -----------------------------main funciton start's ---------------------------
 int main()
 {
-    int n, size = 1000, temp, flag = 1;
+    int n, temp, flag = 1,i,m,size;
     cout << "how many tests you want to carry out : ";
     cin >> n;
-    int i = 0;
-    // vectors to store result of time taken 
+    // vectors to store result of time taken
     vector<double> testv;
-    vector<vector<double>> bv,iv,sv;
-    // vectors to use as unsorted array 
+    vector<vector<double>> bv, iv, sv;
+    // vectors to use as unsorted array
     vector<int> v1, v2, v3;
+    // to measeure the time taken 
+    clock_t start, end;
+    // to write in file 
+    fstream fout;
+    fout.open("result", ios::out);
 
     // main loop to run all the things
-    clock_t start, end;
-    while (i < n)
+    for (int p = 0; p < 3; p++)
     {
-        cout<<"starting "<<i<<"th round.."<<endl;
+         i = 0;
+         m = n;
+         size =1000;
 
-        // to initailize array
-        for (int j = 0; j < size; j++)
+        switch (p)
         {
-            temp = 1 + (rand() % size);
-            v1.push_back(temp);
+        case 0:
+            fout<<"Best case : "<<endl;
+            break;
+        case 1:
+            fout<<endl<<"Avarage case : "<<endl;
+            break;
+        case 2:
+            fout<<endl<<"worst case : "<<endl;
+        }
+        while (i < m)
+        {
+            cout << "starting " << i << "th round.." << endl;
+
+            // to initailize array
+            if (p == 0)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    v1.push_back(j);
+                }
+            }
+            else if (p == 2)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    temp = size - j;
+                    v1.push_back(temp);
+                }
+            }
+            else
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    temp = 1 + (rand() % size);
+                    v1.push_back(temp);
+                }
+            }
+
+            // coping arr1 to arr2 and arr3
+            v2 = v1;
+            v3 = v1;
+
+            // calling bubble sort
+            start = clock();
+            bsort(v1);
+            end = clock();
+            double time_taken1 = (double(end - start) / double(CLOCKS_PER_SEC)) * 1000;
+            testv.push_back(size);
+            cout<<time_taken1<<endl;
+            testv.push_back(time_taken1);
+            bv.push_back(testv);
+            testv.clear();
+
+            // calling insertion sort
+            start = clock();
+            isort(v2);
+            end = clock();
+            double time_taken2 = (double(end - start) / double(CLOCKS_PER_SEC)) * 1000;
+            cout<<time_taken2<<endl;
+            testv.push_back(size);
+            testv.push_back(time_taken2);
+            iv.push_back(testv);
+            testv.clear();
+
+            // calling selection sort
+            start = clock();
+            ssort(v3);
+            end = clock();
+            double time_taken3 = (double(end - start) / double(CLOCKS_PER_SEC)) * 1000;
+            testv.push_back(size);
+            testv.push_back(time_taken3);
+            cout<<time_taken3<<endl;
+            sv.push_back(testv);
+            testv.clear();
+
+            i++;
+            v1.clear();
+            v2.clear();
+            v3.clear();
+            size = size + 2000;
+            cout << i << "th round completed.." << endl << endl;
         }
 
-        // coping arr1 to arr2 and arr3
-        v2 = v1;
-        v3 = v1;
+        // writing data in file + terminal
 
-        // calling bubble sort 
-        start = clock();
-        bsort(v1);
-        end = clock();
-        double time_taken1 = (double(end - start) / double(CLOCKS_PER_SEC))*1000;
-        testv.push_back(size);
-        testv.push_back(time_taken1);
-        bv.push_back(testv);
-        testv.clear();
+        cout << endl << "time taken  by bubble sort : " << endl;
+        fout << endl << "time taken  by bubble sort : " << endl;
+        for (int z = 0; z < bv.size(); z++)
+        {
+            cout << bv[z][0] << " : " << bv[z][1] << endl;
+            fout << bv[z][0] << " : " << bv[z][1] << endl;
+        }
+        cout << endl<< endl;
+        fout << endl<< endl;
 
+        cout << endl<< "time taken  by insertion sort : " << endl;
+        fout << endl<< "time taken  by insertion sort : " << endl;
+        for (int z = 0; z < iv.size(); z++)
+        {
+            cout << iv[z][0] << " : " << iv[z][1] << endl;
+            fout << iv[z][0] << " : " << iv[z][1] << endl;
+        }
+        cout << endl << endl;
+        fout << endl << endl;
 
-        // calling insertion sort 
-        start = clock();
-        isort(v2);
-        end = clock();
-        double time_taken2 = (double(end - start) / double(CLOCKS_PER_SEC))*1000;
-        testv.push_back(size);
-        testv.push_back(time_taken2);
-        iv.push_back(testv);
-        testv.clear();
+        cout << endl << "time taken  by selection sort : " << endl;
+        fout << endl << "time taken  by selection sort : " << endl;
+        for (int z = 0; z < sv.size(); z++)
+        {
+            cout << sv[z][0] << " : " << sv[z][1] << endl;
+            fout << sv[z][0] << " : " << sv[z][1] << endl;
+        }
 
-        // calling selection sort 
-        start = clock();
-        ssort(v3);
-        end = clock();
-        double time_taken3 = (double(end - start) / double(CLOCKS_PER_SEC))*1000;
-        testv.push_back(size);
-        testv.push_back(time_taken3);
-        sv.push_back(testv);
-        testv.clear();
-
-        i++;
-        v1.clear();
-        v2.clear();
-        v3.clear();
-        size = size + 2000;
-        cout<<i<<"th round completed.."<<endl<<endl;
-    }   
-
-    // writing data in file + terminal 
-    fstream fout;
-    fout.open("result",ios::out);
-
-    cout<<endl<<"time taken  by bubble sort : "<<endl;
-    fout<<endl<<"time taken  by bubble sort : "<<endl;
-    for(int i=0; i<bv.size();i++){
-        cout<<bv[i][0]<<" : "<<bv[i][1]<<endl;
-        fout<<bv[i][0]<<" : "<<bv[i][1]<<endl;
-    }
-    cout<<endl<<endl;
-    fout<<endl<<endl;
-
-    cout<<endl<<"time taken  by insertion sort : "<<endl;
-    fout<<endl<<"time taken  by insertion sort : "<<endl;
-    for(int i=0; i<iv.size();i++){
-        cout<<iv[i][0]<<" : "<<iv[i][1]<<endl;
-        fout<<iv[i][0]<<" : "<<iv[i][1]<<endl;
-    }
-    cout<<endl<<endl;
-    fout<<endl<<endl;
-
-    cout<<endl<<"time taken  by selection sort : "<<endl;
-    fout<<endl<<"time taken  by selection sort : "<<endl;
-    for(int i=0; i<sv.size();i++){
-        cout<<sv[i][0]<<" : "<<sv[i][1]<<endl;
-        fout<<sv[i][0]<<" : "<<sv[i][1]<<endl;
+        bv.clear();
+        iv.clear();
+        sv.clear();
     }
     return 0;
 }
