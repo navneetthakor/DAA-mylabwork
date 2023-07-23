d272ef66588e660a4e93e9946bad2c1b6a99275c
navneetthakor <tnavneer8628@gmail.com>
2023-07-23 13:06:57 +0530

    Merge2 ready

--- lab2/merge.cpp
+++ lab2/merge.cpp
@@ -1,76 +1,82 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+void myMerge(vector<int> &v, int mybegin, int mid, int myend)
+{
+    // creating left and right array
+    vector<int> leftv, rightv;
+    for (int i = mybegin; i <= mid; i++)
+    {
+        leftv.push_back(v[i]);
+    }
 
+    for (int i = mid + 1; i <= myend; i++)
+    {
+        rightv.push_back(v[i]);
+    }
 
-void myMerge(vector<int>v, int mybegin, int mid, int myend){
-cout<<"1"<<endl;
-    //creating left and right array
-    vector<int> leftv = {v.begin(),v.begin() + mid};
-    vector<int> rightv = {v.begin() + mid +1, v.end()};
-
-    //creating some required variables
+    // creating some required variables
     int leftindex = 0;
     int righindex = 0;
     int mergeindex = mybegin;
 
-    //loop to merge in v[mybegin .. myend]
-    while(leftindex<leftv.size() && righindex<rightv.size())
+    // loop to merge in v[mybegin .. myend]
+    while (leftindex < leftv.size() && righindex < rightv.size())
     {
-        if(leftv[leftindex] <= rightv[righindex])
+        if (leftv[leftindex] <= rightv[righindex])
         {
             v[mergeindex] = leftv[leftindex];
             leftindex++;
         }
-        else{
+        else
+        {
             v[mergeindex] = rightv[righindex];
-            righindex++;;
+            righindex++;
+            ;
         }
         mergeindex++;
     }
 
-    //copy remaining elements
-    //leftv remaining data
-    while(leftindex < leftv.size())
+    // copy remaining elements
+    // leftv remaining data
+    while (leftindex < leftv.size())
     {
         v[mergeindex] = leftv[leftindex];
         leftindex++;
         mergeindex++;
     }
 
-    //right remaining data
-    while(righindex < rightv.size())
+    // right remaining data
+    while (righindex < rightv.size())
     {
         v[mergeindex] = rightv[righindex];
         righindex++;
         mergeindex++;
     }
-
 }
 
-void mymergeSort(vector<int> v,int mybegin,int myend)
+void mymergeSort(vector<int> &v, int mybegin, int myend)
 {
-    cout<<"2"<<endl;
-    if(mybegin >= myend){
+    if (mybegin >= myend)
+    {
         return;
     }
 
-    int mid = (myend + mybegin)/2;
+    int mid = (mybegin + myend) / 2;
     mymergeSort(v, mybegin, mid);
     mymergeSort(v, mid + 1, myend);
-    myMerge(v,mybegin, mid, myend);
+    myMerge(v, mybegin, mid, myend);
 }
 
-int main(){
-    cout<<"3"<<endl;
-    vector<int> v = {8,6,7,3,1,23};
-    mymergeSort(v,0,6);
-    cout<<"4"<<endl;
-    for(int i=0; i<v.size(); i++)
+int main()
+{
+    vector<int> v = {1, 2, 3, 4, 45, 6, 9, 0};
+    mymergeSort(v, 0, v.size());
+    cout << "After short : " << endl;
+    for (int i = 0; i < v.size(); i++)
     {
-        cout<<v[i]<<endl;
+        cout << v[i] << endl;
     }
 
-return 0;
-
+    return 0;
 }
